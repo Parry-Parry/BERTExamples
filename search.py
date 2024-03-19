9a8963de1e17521f6fd1608fd9825644a97624f3
Andrew Parry <2393265p@student.gla.ac.uk>
2024-03-19 14:21:56 +0000

    super silly

--- search.py
+++ search.py
@@ -3,7 +3,6 @@ if not pt.started():
     pt.init()
 import pandas as pd
 import ir_datasets as irds
-from transformers import AutoTokenizer, AutoModel
 from pyterrier_dr import HgfBiEncoder, NumpyIndex
 from fire import Fire
 
@@ -12,9 +11,7 @@ def embed_queries(dataset : str, out_path : str, subset : int = 0, model : str =
     docpair_lookup = pd.DataFrame(irds.load(lookup).docpairs_iter()).drop_duplicates("query_id").set_index("query_id").doc_id_a.to_dict()
     queries = pd.DataFrame(irds.load(dataset).queries_iter()).rename(columns={"query_id":"qid", "text" : "query"})
    
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    model = AutoModel.from_pretrained(model)
-    hgf = HgfBiEncoder(model, tokenizer, batch_size=batch_size)
+    hgf = HgfBiEncoder.from_pretrained(model, batch_size=batch_size)
 
     pipe = hgf >> index % cutoff
     res = pipe.transform(queries).rename(columns={"qid":"query_id", "docno" : "rel_query_id"})[['query_id', 'rel_query_id']]
